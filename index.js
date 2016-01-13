b2b9b6c1e72bc32d9f685ae14bff10042e94fb40
Eugene Baranchuk <vencedor@gmail.com>
2016-01-13 11:40:56 +0200

    added root index file

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+var datetime_1 = require("./src/datetime");
+exports.default = datetime_1.default;
+__export(require("./src/datetime"));
+//# sourceMappingURL=index.js.map
